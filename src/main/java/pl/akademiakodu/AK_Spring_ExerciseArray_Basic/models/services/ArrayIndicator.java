53de8d7ec4642227107ff70f35f03423ca38faed
Kerawos <Maresowa@gmail.com>
2017-12-04 13:10:59 +0100

    @add comments array indicator

--- src/main/java/pl/akademiakodu/AK_Spring_ExerciseArray_Basic/models/services/ArrayIndicator.java
+++ src/main/java/pl/akademiakodu/AK_Spring_ExerciseArray_Basic/models/services/ArrayIndicator.java
@@ -1,10 +1,21 @@
 package pl.akademiakodu.AK_Spring_ExerciseArray_Basic.models.services;
 
+/**
+ * Import section
+ */
 import org.springframework.stereotype.Service;
 
+/**
+ * Indicate simple request, checks
+ */
 @Service
 public class ArrayIndicator {
 
+    /**
+     * Method responsible for indicate if given string can be parse to integer
+     * @param s as string
+     * @return result of parsing
+     */
     public boolean isInteger(String s) {
         try {
             Integer.parseInt(s);
@@ -16,6 +27,11 @@ public class ArrayIndicator {
         return true;
     }
 
+    /**
+     * Method responsible for indicate if given string array can be parse to integer array
+     * @param unknownArray string array
+     * @return result of parsing
+     */
     public boolean isIntArray(String[] unknownArray){
         for (String o : unknownArray) {
             if (!isInteger(o)){
